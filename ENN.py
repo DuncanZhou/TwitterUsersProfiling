6448a449e8f429de4a3913a1c7c0acea2fd7e362
DuncanZhou <364451395@qq.com>
2017-07-11 10:04:02 +0800

    重新构造了小顶堆

---
+++ ENN.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+'''@author:duncan'''
+
+# 此方法先聚类,再找最近邻元素加入
--- Recursion_Clustering.py
+++ Recursion_Clustering.py
@@ -8,18 +8,27 @@ import Metric as metric
 import heapq
 
 # 定义一个小顶堆
-class MinHeap:
+class MinHeap(object):
 
-    def __init__(self):
-        self.data = []
+    # 允许传入tuple,按照第二个元素比较
+    def __init__(self, initial=None, key=lambda x:x[1]):
+        self.key = key
+        if initial:
+            self._data = [(key(item), item) for item in initial]
+            heapq.heapify(self._data)
+        else:
+            self._data = []
 
-    def Push(self, elem):
-        if len(self.data) == 0:
-            heapq.heappush(self.data,elem)
+    def push(self, item):
+        heapq.heappush(self._data, (self.key(item), item))
 
-        topk_small = self.data[0]
-        if elem[1] < topk_small[1]:
-            heapq.heapreplace(self.data, elem)
+    def pop(self):
+        return heapq.heappop(self._data)[1]
+
+    def size(self):
+        return len(self._data)
+
+
 
 # 递归聚类
 class Recursion_Cluster:
@@ -44,7 +53,7 @@ class Recursion_Cluster:
         iteration = 0
         while iteration < self.Max_iteration:
             old_size = len(s_current)
-            # minHeap = MinHeap()
+            minHeap = MinHeap()
             to_check = []
             # 对每个子的聚类簇继续聚类
             for clusters in cluster:
@@ -56,14 +65,16 @@ class Recursion_Cluster:
                 self.cmethod.features = cluster_vec
                 subcluster,sub_k_seeds,seeds_loss = self.cmethod.Cluster()
                 # 使用最小堆尽然使结果降低了
-                # for seed in seeds_loss.keys():
-                #     minHeap.Push((seed,seeds_loss[seed]))
-                for seed in sub_k_seeds:
-                    to_check.append(seed)
+                for seed in seeds_loss.keys():
+                    minHeap.push((seed,seeds_loss[seed]))
+                # for seed in sub_k_seeds:
+                #     to_check.append(seed)
             # to_check = minHeap.data
             # 对加入的新的元素进行判断
-            for check in to_check:
+            # for check in to_check:
+            for i in range(minHeap.size()):
                 # s_current[check[0]] = self.features[check[0]]
+                check = minHeap.pop()[0]
                 s_current[check] = self.features[check]
                 if metric.metric(self.features,s_current) >= Loss_current:
                     s_current.pop(check)
@@ -72,5 +83,16 @@ class Recursion_Cluster:
             if len(s_current) == old_size:
                 break
         return s_current
+# def test():
+#     # 构造小顶堆
+#     minheap = MinHeap()
+#     lists = [('b',3),('c',1),('a',2)]
+#     for list in lists:
+#         minheap.push(list)
+#     for i in range(minheap.size()):
+#         print minheap.pop()
+#
+# test()
+
 
 
