18228d2132f70ebadcb19d81c34da4d1bec22ad0
DuncanZhou <364451395@qq.com>
2017-07-31 15:44:29 +0800

    重新定义了metric函数

--- Metric.py
+++ Metric.py
@@ -5,9 +5,11 @@
 import Distance as dist
 import DataPrepare as datapre
 import math
+import random
 
 a = 2
 b = 50
+Sampling_Number = 1000
 
 # sigmoid函数,挤压函数
 def sigmoid(x):
@@ -17,7 +19,7 @@ def sigmoid(x):
 def AttributeLoss(origin_features,profile_features):
     loss = 0
     for key in origin_features.keys():
-        list = [dist.distance(origin_features[key],profile_features[u]) for u in profile_features.keys() if profile_features[u][5] == origin_features[key][5]]
+        list = [dist.distance(origin_features[key],profile_features[u]) for u in profile_features.keys()]
         loss += min(list)
     return loss
 
@@ -29,6 +31,8 @@ def DistributionLoss(original_features,profile_features):
             categories[original_features[key][5]] = 1
         else:
             categories[original_features[key][5]] += 1
+    for key in categories.keys():
+        categories[key] = categories[key] * 1.0 / len(original_features)
 
     profile_categories = {}
     for key in profile_features.keys():
@@ -36,10 +40,17 @@ def DistributionLoss(original_features,profile_features):
             profile_categories[profile_features[key][5]] = 1
         else:
             profile_categories[original_features[key][5]] += 1
+
+    for key in profile_categories.keys():
+        profile_categories[key] = profile_categories[key] * 1.0 / len(profile_features)
+
     # 统计损耗
     loss = 0
     for key in categories.keys():
-        loss += pow(categories[key] - profile_categories[key],2)
+        if key not in profile_categories.keys():
+            loss += 1
+        else:
+            loss += pow(categories[key] - profile_categories[key],2)
     return math.sqrt(loss)
 
 # 子集内部差异性
@@ -53,8 +64,6 @@ def Dissimilarity(profile_features):
         else:
             categories[profile_features[key][5]].append(key)
 
-    print categories
-
     # 寻找每个领域与其他领域之间的相似度
     total_similarity = 0
     for key1 in categories.keys():
@@ -67,28 +76,55 @@ def Dissimilarity(profile_features):
                 minimals = []
                 for id1 in targets:
                     minimals.append(min([dist.distance(profile_features[id1],profile_features[id2]) for id2 in other]))
-                similarity += min(minimals)
-        total_similarity += 1 * 1.0 / len(categories) * similarity
+                similarity += 2 * sigmoid(1.0 / min(minimals)) - 1
+        total_similarity +=  similarity
 
-    return total_similarity
+    return total_similarity / (len(categories) - 1)
 
-#　子集的代表性衡量
+# 抽样选取最大和最小距离
+def Sampling(original_features):
+    total_number = len(original_features)
+    ids = original_features.keys()
+    max = min = 0
+    i = 0
+    while i < Sampling_Number:
+        a = random.randint(0,total_number-1)
+        b = random.randint(0,total_number-1)
+        while b == a:
+            b = random.randint(0,total_number-1)
+        if max == min == 0:
+            min = dist.distance(original_features[ids[a]],original_features[ids[b]])
+            max = min
+        else:
+            new_distance = dist.distance(original_features[ids[a]],original_features[ids[b]])
+            if max < new_distance:
+                max = new_distance
+            if min > new_distance:
+                min = new_distance
+        i += 1
+    return max,min
+
+#　代表性子集的代表性衡量
 def metric(origin_features,profile_features):
-    size_profile = len(profile_features.keys())
+    category_number = len(datapre.category_dic)
     total_number = len(origin_features.keys())
-    # 一共分为三个部分,
-    loss = 0
+    # 一共分为三个部分,属性损耗,分布损耗,代表性子集差异性
+    max,min = Sampling(origin_features)
+    loss1 = ((AttributeLoss(origin_features,profile_features)) - total_number * min) / (total_number * max - total_number * min)
+    loss2 = DistributionLoss(origin_features,profile_features) / math.sqrt(category_number)
+    loss3 = Dissimilarity(profile_features) / category_number
+    print loss1,loss2,loss3
+    loss = loss1 + loss2 + loss3
     return loss
-
 
 def test():
     features = datapre.Features()
     profile_features = {}
-    count = 0
+    i = 0
     for key in features.keys():
         profile_features[key] = features[key]
-        count += 1
-        if count > 30:
+        i += 1
+        if i > 20:
             break
-    print Dissimilarity(profile_features)
+    print metric(features,profile_features)
 test()
