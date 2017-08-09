3221f3a1380844dca60fda29a66a852efce8cca9
DuncanZhou <364451395@qq.com>
2017-08-09 18:26:23 +0800

    修改了SA算法,实验证明结果差于贪心算法

--- SAalgo.py
+++ SAalgo.py
@@ -33,8 +33,9 @@ class SAalgo:
         if delta < 0:
             return True
         else:
-            # return random.randint(1,100) <= math.exp((-delta) * 1.0 / temper) * 100
-            return False
+            print math.exp(-delta * 1.0 / temper)
+            return math.exp(-delta * 1.0 / temper) > random.random()
+            # return False
 
     def Search(self):
         # 将人物按领域分类
@@ -48,10 +49,9 @@ class SAalgo:
         # 初始解(贪心算法获得)
         method = greedy.Greedy(self.k,self.features,self.categories,self.epsilon)
         current_profiles = set(method.Search())
-        best_profiles = copy.deepcopy(current_profiles)
-        least_loss = metric.AttributeLoss(self.features,current_profiles)
 
-        while self.temper > 0.1:
+        while self.temper > 1.1:
+            change = False
             # 开始迭代搜索解,对于领域解S,如果S优于当前解则接受领域解,否则以一定概率接受(如此避免了局部最优)
             for profile in current_profiles:
                 # 对其中每个元素的领域元素判断,将profile领域的元素加入队列中
@@ -60,33 +60,32 @@ class SAalgo:
                 for id in tuples:
                     if id not in current_profiles and dist.distance(self.features[id],self.features[profile]) <= self.neighbour:
                         neighbours.put(id)
-                print "有%d个邻居" % len(neighbours)
+                print "有%d个邻居" % neighbours.qsize()
+                new_profiles = copy.deepcopy(current_profiles)
+                new_profiles.remove(profile)
                 # 对其领域进行判断
                 while not neighbours.empty():
                     to_check = neighbours.get()
                     old_loss = metric.AttributeLoss(self.features,current_profiles)
-                    new_profiles = copy.deepcopy(current_profiles)
-                    new_profiles.remove(profile)
                     new_profiles.add(to_check)
                     new_loss = metric.AttributeLoss(self.features,new_profiles)
                     delta = new_loss - old_loss
-                    print old_loss
-                    print new_loss
+                    # print old_loss
+                    # print new_loss
                     flag = self.accept(delta,self.temper)
                     if flag and metric.checkOneTypical(self.features,to_check,new_profiles,self.epsilon):
                         current_profiles = new_profiles
-                        if new_loss < least_loss:
-                            best_profiles = current_profiles
-                            least_loss = new_loss
+                        change = True
                         break
-            if current_profiles == best_profiles:
+                    new_profiles.remove(to_check)
+            if not change:
                 # 没有改变
                 break
             self.temper *= self.dec
-        return list(best_profiles)
+        return list(current_profiles)
 
 def test():
-    method = SAalgo(20,datapre.Features(),datapre.CategoriesDistribution(),0.05,2,1000,0.9)
+    method = SAalgo(20,datapre.Features(),datapre.CategoriesDistribution(),0.05,0.5,10,0.9)
     profiles = method.Search()
     print "Attribute Loss is"
     print metric.AttributeLoss(method.features,profiles)
