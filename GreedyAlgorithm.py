a6bd092e52df978e8943b2437bd132b992361a11
DuncanZhou <364451395@qq.com>
2017-08-11 14:24:58 +0800

    重新修改了贪心算法

--- GreedyAlgorithm.py
+++ GreedyAlgorithm.py
@@ -79,12 +79,56 @@ class Greedy:
         # print len(profiles)
         return list(profiles)
 
+    # 贪心算法保证k个情况
+    def SearchWithK(self):
+        people = self.People()
+        profiles = set()
+        has_category = set()
+        category_loss = {}
+        count = 0
+        while count < self.k:
+            results = {}
+            for category in self.categories.keys():
+                if category in has_category:
+                    continue
+                result = {}
+                # tuples为该领域所有的人
+                tuples = people[category]
+                for id in tuples:
+                    if id not in profiles:
+                        profiles.add(id)
+                        if category not in category_loss.keys():
+                            result[id] = metric.AttributeLossByDomain(self.features, profiles,category)
+                        else:
+                            result[id] = metric.AttributeLossByDomain(self.features,profiles,category) - category_loss[category]
+                        profiles.remove(id)
+                results[(min(result.items(),key=lambda key:key[1]))[0]] = (min(result.items(),key=lambda key:key[1]))[1]
+
+            to_add = (min(results.items(),key=lambda key:key[1]))[0]
+            print self.features[to_add][5]
+            # 检查该领域有没有超出人数限制
+            number = 0
+            category = self.features[to_add][5]
+            for profile in profiles:
+                if self.features[profile][5] == category:
+                    number += 1
+            if number < ((int)(self.k * self.categories[category]) + 1):
+                # 可以加入
+                profiles.add(to_add)
+                category_loss[category] = metric.AttributeLossByDomain(self.features,profiles,category)
+                count += 1
+                if number + 1 == ((int)(self.k * self.categories[category]) + 1):
+                    has_category.add(self.features[to_add][5])
+            print count
+        return profiles
+
     # 对非典型的元素进行替换
     def Replace(self,target,profiles):
         # people为每个领域的用户集合
         people = self.People()
         # 对target进行替换(在其所属领域寻找满足领域典型的,同样使用贪心算法)
         index = profiles.index(target)
+        old_element = profiles[index]
         results = {}
         for person in people[self.features[target][5]]:
             if person != target and person not in profiles and metric.checkOneTypical(self.features,person,profiles,self.epsilon):
@@ -92,47 +136,49 @@ class Greedy:
                 results[person] = metric.AttributeLossByDomain(self.features,set(profiles),self.features[target][5])
         new_element = (min(results.items(),key=lambda key:key[1]))[0]
         self.replace[target] = new_element
+        profiles[index] = old_element
         # print new_element
         return new_element
 
     # 递归替换非领域典型元素算法
     def SearchRecursion(self,start,current_profiles):
+        # print "running"
         # 递归终止条件(已经)
         if metric.checkAllTypical(self.features,current_profiles,self.epsilon):
             # print "找到一个可行解"
             if self.min_loss == 0 or metric.AttributeLoss(self.features,set(current_profiles)) < self.min_loss:
                 self.min_loss = metric.AttributeLoss(self.features,set(current_profiles))
                 self.best_profiles = set(current_profiles)
-            return
-        # 减枝
-        if metric.AttributeLoss(self.features,set(current_profiles)) > self.min_loss:
-            return
+                print self.min_loss
+            return True
+        # 不是典型,而最小的损耗已经大于最小损耗,不必再向下搜索了
+        if self.min_loss != 0 and metric.AttributeLoss(self.features,set(current_profiles)) > self.min_loss:
+            return True
 
         i = start
         while i < len(current_profiles):
-            old_element = current_profiles[i]
             # print old_element
             # 如果非典型就替换该元素
             if not metric.checkOneTypical(self.features,current_profiles[i],set(current_profiles),self.epsilon):
+                new_profiles = copy.deepcopy(current_profiles)
                 # 先在已经计算过的字典中去寻找
-                if current_profiles[i] in self.replace.keys():
-                    current_profiles[i] = self.replace[current_profiles[i]]
-                    if not metric.checkOneTypical(self.features,current_profiles[i],current_profiles,self.epsilon):
-                        # 重新寻找,替换这一步比较慢
-                        current_profiles[i] = self.Replace(current_profiles[i],current_profiles)
+                if current_profiles[i] in self.replace.keys() and metric.checkOneTypical(self.features,current_profiles[i],new_profiles,self.epsilon):
+                    new_profiles[i] = self.replace[current_profiles[i]]
                 else:
-                    current_profiles[i] = self.Replace(current_profiles[i],current_profiles)
+                    new_profiles[i] = self.Replace(current_profiles[i],new_profiles)
+                # 继续向下寻找
+                flag = self.SearchRecursion(i + 1,new_profiles)
+                if flag == True:
+                    # 已无需向下搜索
+                    break
             #
             # print current_profiles
-            # 继续向下寻找
-            self.SearchRecursion(i + 1,current_profiles)
+
             # print self.min_loss
             # 如果被替换了,还替换回来
-            current_profiles[i] = old_element
             # 继续搜索
             i += 1
-            print self.min_loss
-        return
+        return False
 
 
     # 删除多出来的用户
@@ -160,25 +206,26 @@ class Greedy:
     def SearchWithReplace(self):
         # 第一步,在没有领域典型的条件得到的贪心最优值
         current_profiles = self.SearchWithoutConstraints()
-
+        # self.best_profiles = self.SearchWithK()
+        # print len(self.best_profiles)
         # 贪心排除多余的
         self.best_profiles = self.Delete(set(current_profiles))
 
         # 第二步,排除不够典型的,统计贪心算法求得的解中有哪些不够典型的
-
         self.SearchRecursion(0,list(self.best_profiles))
 
         return self.best_profiles
 
     # 贪心寻找(考虑了领域典型条件,边贪心寻找,边判断条件)
     def SearchWithConstraints(self):
+        people = self.People()
         # 每次并入使得目标函数最小化
         profiles = set()
         for category in self.categories.keys():
             # p_number为该领域需要的人数
             p_number = (int)(self.k * self.categories[category]) + 1
             # tuples为该领域所有的人
-            tuples = [id for id in self.features.keys() if self.features[id][5] == category]
+            tuples = people[category]
             # 迭代p_number次
             count = 0
             has_checked = set()
@@ -211,8 +258,8 @@ def test():
     start_time = time.time()
     method = Greedy(30,datapre.Features(),datapre.CategoriesDistribution(),0.0499)
     # profiles = method.SearchWithoutConstraints()
-    # profiles = method.SearchWithConstraints()
-    profiles = method.SearchWithReplace()
+    profiles = method.SearchWithConstraints()
+    # profiles = method.SearchWithReplace()
     # print len(profiles)
     end_time = time.time()
     print "cost %f s" % (end_time - start_time)
