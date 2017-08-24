808bb88ffd3a026ee027e3e73957aeddf48e393a
DuncanZhou <364451395@qq.com>
2017-08-24 15:53:59 +0800

    从文本中插入用户关系

--- Neo4j-Twitter.py
+++ Neo4j-Twitter.py
@@ -3,6 +3,8 @@
 '''@author:duncan'''
 
 import TwitterWithNeo4j as neo4j
+import DataPrepare as datapre
+import os
 
 # 创建结点
 def CreateNodes(path):
@@ -50,6 +52,33 @@ def SearchUsersByCategory(categroy):
     users = neo4j.SearchUsersByCategory(categroy)
     return users
 
+# 读取文本中该用户follow的id,并且这些id都在数据集中
+def GetUserFollowing(path,total_users):
+    # total_users 是一个集合
+    # 打开一个文件,读取其中该用户所有关注的用户
+    results = set()
+    with open(path,"rb") as f:
+        lines = f.readlines()
+        for line in lines:
+            ids = set(line.split(" "))
+            results = results | (ids & total_users)
+    return results
+
+# 对文件夹下的用户建立关系
+def InsertFollowsFromFiles(path,total_users):
+    # 先读取path下的文件名
+    files = os.listdir(path)
+    ids = map(lambda file:file.replace(".txt",""),files)
+    # 对每个id进行读取其follows的id,然后插入到neo-4j中
+    for id in ids:
+        results = GetUserFollowing(path + "/" + id + ".txt",total_users)
+        print "插入%d条关系" % len(results)
+        # 插入到图数据库中
+        for result in results:
+            InsertFollowsRel(id,result)
+
+
+
 # 从mysql数据库中将标准人物样本库的人物关系存入neo4j中
 # mysql数据格式:userid1,userid2,followed_by,following
 # def InsertRelsToNeoFromMysql(table="relation_temp"):
@@ -65,8 +94,10 @@ def SearchUsersByCategory(categroy):
 
 if __name__ == '__main__':
     # 初始化操作,最开始执行一次,用于创建结点
-    DeleteNodesAndRels()
-    Initial()
+    # DeleteNodesAndRels()
+    # Initial()
 
+    # users = datapre.GetUsersId()
+    InsertFollowsFromFiles("/home/duncan/friends/")
     # 插入mysql中所有的关系
     # InsertRelsToNeoFromMysql()
