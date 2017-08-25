2433634911a97b977decf6fbf16ef6879f8cc9c5
DuncanZhou <364451395@qq.com>
2017-08-25 22:32:50 +0800

    将结点和关系都存储到csv文件中

--- LoadRelationships.py
+++ LoadRelationships.py
@@ -2,6 +2,7 @@
 #-*-coding:utf-8-*-
 '''@author:duncan'''
 
+from pymongo import MongoClient
 import os
 import DataPrepare as datapre
 # 将用户结点之间的关系处理成CSV文件,在批量导入neo-4j中
@@ -27,6 +28,25 @@ def LoadToCSV(path):
             # 获取到该id所有的friends,写入到文件中
             for friend in friends:
                 f.write(id)
+                f.write(" ")
+                f.write(friend)
+                f.write("\n")
+            count += 1
+            print "finished %d users" % count
+
+        # 将Mongo中的关系也写入csv文件中
+        # connect to mongodb localhost
+        client = MongoClient("127.0.0.1",27017)
+        # define the name of database
+        db = client.relationships
+        infos = db.famous_friends.find()
+        for info in infos:
+            id = info["_id"]
+            friends = info["friends"]
+            friends = set(map(lambda friend:str(friend),friends))
+            friends = friends & users
+            for friend in friends:
+                f.write(str(id))
                 f.write(" ")
                 f.write(friend)
                 f.write("\n")
