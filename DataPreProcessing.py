8a00d7f2a051f1730ca5a78515e23a708b6e6e95
DuncanZhou <364451395@qq.com>
2018-05-21 15:16:02 +0800

    修正人物关系获取方法

--- DataPreProcessing.py
+++ DataPreProcessing.py
@@ -8,13 +8,7 @@ import csv
 import TwitterWithNeo4j as neo4j
 import cPickle as pickle
 import networkx as nx
-import matplotlib.pyplot as plt
-from networkx.algorithms.community import k_clique_communities
-from networkx.algorithms.community.asyn_lpa import asyn_lpa_communities
-from networkx.algorithms.community.asyn_fluidc import asyn_fluidc
-from networkx.algorithms.community.quality import coverage
-from networkx.algorithms.community.quality import performance
-from networkx.algorithms.community.community_utils import is_partition
+import Initial as init
 from networkx.classes.function import all_neighbors
 import time
 import pandas as pd
@@ -35,15 +29,26 @@ class Twitter_User:
         self.protected = protected
         self.verified = verified
 
+class Weibo_User:
+    def __init__(self,id,friends_count,followers_count,statuses_count,favourtites_count,gender,verified,city,urank,category):
+        self.friends_count = friends_count
+        self.followers_count = followers_count
+        self.statuses_count = statuses_count
+        self.favourites_count = favourtites_count
+        self.gender = gender
+        self.verified = verified
+        self.city = city
+        self.urank = urank
+        self.category = category
 
 # 数据库连接
 def Connection():
     conn = MySQLdb.connect(
         host= "192.168.131.191",
         port = 3306,
-        user= "duncan",
-        passwd= "123",
-        db = "twitter_users",
+        user= "root",
+        passwd= "",
+        db = "weibo",
         # host= "127.0.0.1",
         # port = 3306,
         # user= "root",
@@ -64,20 +69,20 @@ def Close(conn,cursor):
     conn.close()
 
 # 获取所有用户的特征向量
-def GetAllUsersFeature(table="newStandardUsers"):
+def GetAllUsersFeature(table="users"):
     conn,cursor = Connection()
     cursor.execute("SELECT * FROM %s" % table)
     datas = cursor.fetchall()
     users = []
     for data in datas:
-        twitter_user = Twitter_User(data['user_id'],int(data['followers_count']),int(data['friends_count']),int(data['statuses_count']),int(data['favourites_count']),float(data['activity']),float(data['influence_score']),data['time_zone'],data['protected'],data['verified'],data['category'])
+        weibo_user = Weibo_User(data['id'],int(data['friends_count']),int(data['followers_count']),int(data['statuses_count']),int(data['favourites_count']),int(data['gender']),int(data['verified']),data['city'],data['urank'],data['category'])
         # users.append(twitter_user)
-        users.append((data['user_id'],int(data['followers_count']),int(data['friends_count']),int(data['statuses_count']),int(data['favourites_count']),float(data['activity']),float(data['influence_score']),data['time_zone'],data['protected'],data['verified'],data['category']))
+        users.append((data['id'],int(data['friends_count']),int(data['followers_count']),int(data['statuses_count']),int(data['favourites_count']),int(data['gender']),int(data['verified']),data['city'],data['urank'],data['category']))
     Close(conn,cursor)
     return users
 
 # 获取某个领域的所有用户的特征向量
-def GetUsersFeature(category,table="newStandardUsers"):
+def GetUsersFeature(category,table="users"):
     conn,cursor = Connection()
     cursor.execute("SELECT * FROM %s where category = '%s'" % (table,category))
     datas = cursor.fetchall()
@@ -85,19 +90,26 @@ def GetUsersFeature(category,table="newStandardUsers"):
     for data in datas:
         # twitter_user = Twitter_User(data['user_id'],int(data['followers_count']),int(data['friends_count']),int(data['statuses_count']),int(data['favourites_count']),float(data['activity']),float(data['influence_score']),data['time_zone'],data['protected'],data['verified'],data['category'])
         # users.append(twitter_user)
-        users.append((data['user_id'],int(data['followers_count']),int(data['friends_count']),int(data['statuses_count']),int(data['favourites_count']),float(data['activity']),float(data['influence_score']),data['time_zone'],data['protected'],data['verified'],data['category']))
+        users.append((data['id'],int(data['friends_count']),int(data['followers_count']),int(data['statuses_count']),int(data['favourites_count']),int(data['gender']),int(data['verified']),data['city'],data['urank'],data['category']))
     Close(conn,cursor)
     return users
 
 # 从csv中获取某一领域用户的数据
-def GetUsersFromCSV(category,path="Users.csv"):
-    path = category + path
+def GetUsersFromCSV(path="TotalUsers.csv"):
+    # path = "users/" + category + path
+    data = pd.read_csv(path)
+    return data
+
+# 从csv中获取某一领域用户的数据
+def GetUsersFromCSVByCategory(category,path="Users.csv"):
+    # path = "users/" + category + path
+    path = "users/" + category + path
     data = pd.read_csv(path)
     return data
 
 # 得到特征属性
 def GetFeatureColumns():
-    return ['followers','friends','statuses','favourites','activity','influence','location','verified']
+    return ['followers','friends','statuses','favourites','activity','influence','verified']
 
 # 计算两个用户之间的代表性
 # u,v为两个用户的userid
@@ -113,26 +125,86 @@ def r(u,v):
 def WriteIntoCSV(users):
     with open("/home/duncan/TotalUsers.csv","wb") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['userid','followers','friends','statuses','favourites','activity','influence','location','protected','verified','category'])
+        writer.writerow(['userid','friends','followers','statuses','favourites','gender','verified','city','urank','category'])
         writer.writerows(users)
 
-def GetRelationships(catgegory):
-    driver,session = neo4j.Conn()
-    users = GetUsersFeature(catgegory)
-    id_list = [user[0] for user in users]
+def WriteIntoCSVByCategory(category):
+    users = GetUsersFeature(category)
+    with open("users/%sUsers.csv" % category,"wb") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['userid','friends','followers','statuses','favourites','gender','verified','city','urank','category'])
+        writer.writerows(users)
+
+def GetRelationships(users,category):
+    # driver,session = neo4j.Conn()
+    # users = GetUsersFeature(catgegory)
+    # id_list = [user[0] for user in users]
+    # relationships = []
+    # for i in xrange(len(id_list)):
+    #     followings = set(neo4j.GetFollowings(driver,session,id_list[i]))
+    #     j = i + 1
+    #     while j < len(id_list):
+    #         if id_list[j] in followings:
+    #             relationships.append([i,j])
+    #         j += 1
+    # rel_file = open("%s_rels" % catgegory,'wb')
+    # ids_file = open("%s_ids" % catgegory,'wb')
+    # pickle.dump(relationships,rel_file,True)
+    # pickle.dump(id_list,ids_file,True)
+    # print len(relationships)
+    users = GetUserByCategory(users,category)
+    conn,cursor = Connection()
+    id_list = []
     relationships = []
-    for i in xrange(len(id_list)):
-        followings = set(neo4j.GetFollowings(driver,session,id_list[i]))
-        j = i + 1
-        while j < len(id_list):
-            if id_list[j] in followings:
-                relationships.append([i,j])
-            j += 1
-    rel_file = open("%s_rels" % catgegory,'wb')
-    ids_file = open("%s_ids" % catgegory,'wb')
+    i = 0
+    while i < len(users):
+        id_list.append(users.iloc[i]['userid'])
+        i += 1
+    ids = set(id_list)
+    # 对已加入的点无需再加入
+    has = set()
+    for id in id_list:
+        # 将id关注的结点加入
+        cursor.execute("select tuid from relationships where suid = '%s'" % id)
+        results = cursor.fetchall()
+        if len(results) == 0:
+            continue
+        else:
+            count = 0
+            while count < len(results):
+                tuid = int(results[count]['tuid'])
+                # 加入时需要判断tuid是否在同一个领域内,如果之前已经加入过了则无须再加入了
+                if tuid not in ids or tuid in has:
+                    count += 1
+                    continue
+                relationships.append([id,tuid])
+                count += 1
+        has.add(id)
+    Close(conn,cursor)
+    rel_file = open("Graph/%s_rels" % category,'wb')
+    ids_file = open("Graph/%s_ids" % category,'wb')
     pickle.dump(relationships,rel_file,True)
     pickle.dump(id_list,ids_file,True)
     print len(relationships)
+
+# 得到所有用户的关系
+# def GetRelationships():
+#     driver,session = neo4j.Conn()
+#     users = pd.read_csv("TotalUsers.csv")
+#     id_list = users['userid']
+#     relationships = []
+#     for i in xrange(len(id_list)):
+#         followings = set(neo4j.GetFollowings(driver,session,id_list[i]))
+#         j = i + 1
+#         while j < len(id_list):
+#             if id_list[j] in followings:
+#                 relationships.append([i,j])
+#             j += 1
+#     rel_file = open("all_users_rels",'wb')
+#     ids_file = open("all_users_ids",'wb')
+#     pickle.dump(relationships,rel_file,True)
+#     pickle.dump(id_list,ids_file,True)
+#     print len(relationships)
 
 # 社区发现
 def CommunityDetection(id_list_path,rels_path):
@@ -157,59 +229,82 @@ def CommunityDetectionByNX(id_list_path,rels_path):
     # cliques = nx.graph_clique_number(g)
     # print len(community)
     community = nx.algorithms.community.girvan_newman(g)
-    print sorted(map(sorted, next(community)))
+    res = sorted(map(sorted, next(community)))
+    print res
+    all_users_graph = open("all_users_grapha","wb")
+    pickle.dump(res,all_users_graph,True)
 
 def Communities():
     categories = ["Politics","Sports","Military","Entertainment","Agriculture","Technology","Economy","Education","Religion"]
     # for category in categories:
     #     GetRelationships(category)
-    category = "Sports"
+    category = "Religion"
     # CommunityDetection("%s_ids" % category,"%s_rels" % category)
     start_time = time.time()
-    CommunityDetectionByNX("%s_ids" % category,"%s_rels" % category)
+    CommunityDetectionByNX("Graph/%s_ids" % category,"Graph/%s_rels" % category)
     end_time = time.time()
     print "cost %f seconds" % (end_time - start_time)
 
 
-def repre(users,column,feature):
+def repre(users,index,feature):
     rows_num = len(users.index)
-    row = np.array(users.iloc[column][feature]).astype(float)
-    location = users.loc[column]['location']
+    row = np.array(users.iloc[index][feature]).astype(float)
     rows = np.tile(row,(rows_num,1)).astype(float)
     d = np.asarray(users[feature])
     #     temp = np.sum((rows - d) ** 2,axis=1)
     pre = (rows - d) ** 2
-    # 离散值计算
-    location1 = np.full(rows_num,location,int)
-    location2 = np.asarray(users['location'])
-    location_results = np.zeros(rows_num)
-    location_results[location1 == location2] = 1
-    location_results = location_results.reshape(rows_num,1)
-    # 和连续值按列拼接
-    temp = np.hstack((pre,location_results))
-    temp = np.sqrt(np.sum(temp,axis=0))
+
+    temp = np.sum(pre,axis=1)
     res = 2 / (np.exp(1 / (-temp ** 0.5)) + 1) - 1
-    print res
     return res
+
+# 取某个领域的用户
+def GetUserByCategory(users,category):
+    temp = users['category'] == category
+    users = users[temp == True]
+    return users
 
 # 计算代表性矩阵
 def GetRepreMatrix(users,category):
-    feature = GetFeatureColumns()
+    users = GetUserByCategory(users,category)
+    feature = ['friends','followers','statuses','favourites','gender','verified','city','urank']
 
     # 先做归一化处理
     users[feature] = (users[feature] - users[feature].min()) / (users[feature].max() - users[feature].min())
+    users = users.fillna(0)
     res = repre(users,0,feature)
     i = 1
     while i < len(users.index):
         res = np.vstack((res,repre(users,i,feature)))
         i += 1
     R = np.matrix(res)
-    np.save("%sRepresentativeMatrix.npy" % category,R)
+    np.save("R/%sRepresentativeMatrix.npy" % category,R)
 
 def test():
-    categories = ["Politics","Sports","Military","Entertainment","Agriculture","Technology","Economy","Education","Religion"]
-    users = GetUsersFromCSV("Sports")
-    GetRepreMatrix(users,"Sports")
+    categories = ["Actor","Singer","Director","Player","Common"]
+
+    # 将所用用户写入csv文件
+    # users = GetAllUsersFeature()
+    # WriteIntoCSV(users)
+
+    # users = GetUsersFromCSV()
+
+    # WriteIntoCSVByCategory('Common')
+
+    users = GetUsersFromCSVByCategory('Actor')
+    # print len(users)
+    # WriteIntoCSVByCategory('Singer')
+    # 得到代表性矩阵
+    # GetRepreMatrix(users,"Singer")
+
+    # 得到关系
+    GetRelationships(users,'Actor')
+    # GetRelationships()
+    # users = GetUsersFromCSV(category)
+    # CommunityDetectionByNX("Graph/all_users_ids","Graph/all_users_rels")
+    # users,R,id_list,g = init.Init(category)
+    # GenerateNeighbours(category,id_list,g)
+    # GetRepreMatrix(users,category)
     # print users.index
 
     # features = GetFeatureColumns()
